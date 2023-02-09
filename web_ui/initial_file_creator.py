d0d388f4602a07efe65422ccd4a8f81a4d67d4a0
sean1832 <sean1832725142@gmail.com>
2023-02-10 04:28:05 +1100

    add useful console feedback

--- web_ui/initial_file_creator.py
+++ web_ui/initial_file_creator.py
@@ -7,9 +7,10 @@ user_dir = r'.user'
 def create():
     # create brain data
     util.create_json_not_exist(f'{user_dir}/brain-data.json', [])
+    print(f'brain data created: {user_dir}/brain-data.json')
     # create brain memo
     util.create_json_not_exist(f'{user_dir}/brain-memo.json',
                                {'note_dir': '', 'delimiter': '', 'append_mode': 'False', 'force_mode': 'False'})
-
+    print(f'brain memo file created: {user_dir}/brain-memo.json')
 
 create()
