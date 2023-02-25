1e0acb459abe220bb45d836d165b8a0ab2c3ba4a
sean1832 <sean1832725142@gmail.com>
2023-02-25 15:30:16 +1100

    fix: change default stream mode to false

--- Seanium_Brain.py
+++ Seanium_Brain.py
@@ -91,7 +91,7 @@ with st.sidebar:
                                       "will it be."))
         enable_stream = st_toggle.st_toggle_switch(_('Stream (experimental)'),
                                                    default_value=util.read_json_at(INFO.BRAIN_MEMO, 'enable_stream',
-                                                                                   True))
+                                                                                   False))
 
         if not enable_stream:
             chunk_count = st.slider(_('Answer count'), 1, 5, value=util.read_json_at(INFO.BRAIN_MEMO, 'chunk_count', 1),
